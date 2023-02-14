e73817b2c24909e53594532c43a192b530fd3969
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-14 10:28:35 +0200

    feat: add home page

---
+++ src/data/posts.ts
@@ -0,0 +1,49 @@
+export const posts = [
+  {
+    uid: 'como-utilizar-hooks',
+    first_publication_date: '15 Mar 2021',
+    data: {
+      title: 'Como utilizar Hooks',
+      subtitle: 'Pensando em sincronização em vez de ciclos de vida',
+      author: 'Joseph Oliveira',
+    },
+  },
+  {
+    uid: 'criando-um-app-cra-do-zero',
+    first_publication_date: '19 Abr 2021',
+    data: {
+      title: 'Criando um app CRA do zero',
+      subtitle:
+        'Tudo sobre como criar a sua primeira aplicação utilizando Create React App',
+      author: 'Danilo Vieira',
+    },
+  },
+  {
+    uid: 'como-utilizar-hooks-2',
+    first_publication_date: '15 Mar 2021',
+    data: {
+      title: 'Como utilizar Hooks',
+      subtitle: 'Pensando em sincronização em vez de ciclos de vida',
+      author: 'Joseph Oliveira',
+    },
+  },
+  {
+    uid: 'criando-um-app-cra-do-zero-2',
+    first_publication_date: '19 Abr 2021',
+    data: {
+      title: 'Criando um app CRA do zero',
+      subtitle:
+        'Tudo sobre como criar a sua primeira aplicação utilizando Create React App',
+      author: 'Danilo Vieira',
+    },
+  },
+  {
+    uid: 'como-utilizar-hooks-3',
+    first_publication_date: '15 Mar 2021',
+    data: {
+      title: 'Como utilizar Hooks',
+      subtitle: 'Pensando em sincronização em vez de ciclos de vida',
+      author: 'Joseph Oliveira',
+    },
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import { posts as rawPosts } from '../data/posts';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -15,22 +19,72 @@ interface Post {
   };
 }
 
+interface FormattedPost {
+  slug: string;
+  createdAt: string;
+  title: string;
+  subtitle: string;
+  author: string;
+}
+
 interface PostPagination {
   next_page: string;
   results: Post[];
 }
 
 interface HomeProps {
-  postsPagination: PostPagination;
+  posts: FormattedPost[];
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }: HomeProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
+      <main className={styles.content}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href={`/post/${post.slug}`} key={post.slug}>
+              <a className={styles.post}>
+                <strong>{post.title}</strong>
+                <p>{post.subtitle}</p>
+                <div className="info">
+                  <time>
+                    <FiCalendar />
+                    {post.createdAt}
+                  </time>
+                  <span>
+                    <FiUser />
+                    {post.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+        <button type="button" className={styles.morePostsButton}>
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  // const prismic = getPrismicClient({});
+  // const postsResponse = await prismic.getByType(TODO);
 
-//   // TODO
-// };
+  return {
+    props: {
+      posts: rawPosts.map(post => ({
+        slug: post.uid,
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+        createdAt: post.first_publication_date,
+      })) as FormattedPost[],
+    },
+    revalidate: 60 * 60, // ONE HOUR
+  };
+};
